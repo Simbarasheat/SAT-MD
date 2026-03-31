468213ad3a5fa53621429263802950b064947c8c
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-03-31 21:31:04 +0200

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    prefix: '.',
+    owner: 'Simbarashe Tembo .A.',
+    botName: 'SAT MD BOT',
+    mode: 'public',
+    version: '4.0 FULL',
+    apiKeys: {
+        openai: process.env.OPENAI_KEY
+    }
+};
